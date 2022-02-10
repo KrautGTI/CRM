d4dcba3c8141536bb686e533e8e347d87d40fd0f
KrautGTI <doyle@elansolar.com>
2022-02-10 16:40:47 -0700

    Add files via upload

---
+++ functions/src/common.ts
@@ -0,0 +1,12 @@
+export const enum Status {
+    PENDING = "pending",
+    READY = "ready",
+    DONE = "done",
+    EXPIRED = "expired"
+}
+
+export const enum ProposalPreference {
+    PDF = "pdf",
+    PHONE = "phone",
+    INPERSON = "inperson"
+}
